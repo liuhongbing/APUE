566e0c72c9d827213f75ed9b98739ea091586afe
liuhongbing <liuhongbing10000@gmail.com>
2014-11-07 18:13:13 +0800

    添加错误判断

--- intro/nobuffcp.c
+++ intro/nobuffcp.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
 
 #define BUFSIZE 4096
@@ -6,10 +8,27 @@
 int main(void)
 {
     int n;   
-    char buf[BUFFSIZE];
+    char buf[BUFSIZE];
     
-    n = read(0, buf, BUFSIZE);
-    write(1, buf, n);
+    // On error, -1 is returned
+    // End of file, 0 is returned
+    // The number of bytes read is returned smaller 
+    // than the number of bytes requested not an error
+    while((n = read(0, buf, BUFSIZE)) > 0)
+    {
+        
+        if(write(1, buf, n) < 0)
+        {
+            printf("read error: %s\n", strerror(errno));
+            return -1;
+        }
+    }
+
+    if( n < 0)
+    {
+        printf("read error: %s\n", strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
